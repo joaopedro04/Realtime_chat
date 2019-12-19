053cdab7b591863af2ee8b113c8bde5f00464921
Joao Pedro <50024295+joaopedro04@users.noreply.github.com>
2019-12-19 00:58:40 -0300

    Revert "initial commit"

--- server.js
+++ server.js
@@ -10,30 +10,18 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 
-app.use('/', (req, res) => {
+app.use('/#/', (req, res) => {
     res.render('index.html');
 });
 
-let messages = [];
-
-io.on('connection', (socket) => {
-    console.log(' ');
-    console.log('############################');
-    console.log('Socket conectado');
-    console.log('User_id => ', socket.id);
-
-    socket.emit('previousMessages', messages);
-
-    socket.on('sendObj', data =>{
-        console.log('socket.on recebe --->', data);
-        // let messages = [];
-        messages.push(data);
-        console.log('array de mensagens ---->', messages);
-
-        socket.broadcast.emit('receivedMessages', data);
-    });
+io.on('connection', sockets => {
+    io.socket.get();
+    console.log('Socket conectado!!!');
 });
 
 
-server.listen(3000);
-console.log('Servidor ONLINE em http://localhost:3000');
+
+server.listen(3000, () => {
+    console.log(' ');
+    console.log("###### o server está rodando em https://localhost:3000 #######");
+});
